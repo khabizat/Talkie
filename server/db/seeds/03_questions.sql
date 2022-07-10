605b9cb07a6e5d1c03a7c0cf60c7117755c07838
Khabizat Sharapudinova <habizat22@gmail.com>
2022-07-10 01:13:51 +0000

    Delete whitespace

--- server/db/seeds/03_questions.sql
+++ server/db/seeds/03_questions.sql
@@ -6,11 +6,11 @@ VALUES
 ('What is a doubly-linked list (DLL)? What are its applications?', '2022-07-08', 1, 1),
 
 -- Seed Daniel (user #2) / tag #2 JavaScript
-('Explain how this works in JavaScript', '2022-07-08', 2, 2 ),
+('Explain how this works in JavaScript', '2022-07-08', 2, 2),
 ('Describe event bubbling', '2022-07-08', 2, 2),
 
 -- Seed Bella (user #3) / tag #2 JavaScript / tag #4 CSS / tag#1 Data Structures and Algorithms
-('What is a closure, and how/why would you use one?', '2022-07-07', 3, 2 ),
-('What is the difference between .call and .apply?', '2022-07-01', 3, 2 ),
+('What is a closure, and how/why would you use one?', '2022-07-07', 3, 2),
+('What is the difference between .call and .apply?', '2022-07-01', 3, 2),
 ('How is responsive design different from adaptive design?', '2022-07-03', 3, 4),
-('What is a stack? What are the applications of stack?', '2022-07-02', 3, 1 );
+('What is a stack? What are the applications of stack?', '2022-07-02', 3, 1);
