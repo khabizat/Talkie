0306dfd1faac1cb8eee1f04293155966ac891f89
Khabizat Sharapudinova <habizat22@gmail.com>
2022-07-10 02:27:59 +0000

    Add Question page structure

--- client/src/components/HomeWelcome.jsx
+++ client/src/components/HomeWelcome.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Button from "./Button";
-
+import { useHistory } from "react-router-dom";
 
 export default function HomeWelcome() {
-
+  const history = useHistory();
   const handleStartButton = () => {
-    
+    history.push("/questions");
   }
   return <section className="main-content">
     <main className="home-welcome">
