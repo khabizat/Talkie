15936c2d3eadd7d7d629e79db7f7c272816e7dc8
JeongminJamie <sksrhcalsu@gmail.com>
2022-07-11 16:14:46 -0400

    can see tags name on the questionsPage

--- client/src/components/Comments/CommentList.jsx
+++ client/src/components/Comments/CommentList.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import CommentListItem from "./CommentListItem";
 
 export default function CommentList(props) {
   const { selectedAnswer } = props;
   const comments = selectedAnswer.map((sA) => {
     return (
-      <section>
-        <div>{sA.user_id}</div>
-        <div>{sA.comment}</div>
-        <div>{sA.timestamp}</div>
-      </section>
+      <CommentListItem
+        user_id={sA.user_id}
+        comment={sA.comment}
+        timestamp={sA.timestamp}
+      />
     );
   });
 
--- client/src/components/Questions/QuestionsPage.jsx
+++ client/src/components/Questions/QuestionsPage.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuestionList from "./QuestionList";
 import EachQuestionPage from "./EachQuestionPage";
+import axios from "axios";
 
 export default function QuestionsPage() {
+  const [tags, setTags] = useState(null);
+  const [tagId, setTagId] = useState(null);
   const [questionId, setQuestionId] = useState(null);
 
   const history = useHistory();
@@ -12,8 +15,29 @@ export default function QuestionsPage() {
     history.push("/questions/new");
   };
 
+  const getTags = () => {
+    axios.get("/api/tags").then((response) => {
+      setTags(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getTags();
+  }, []);
+
   return (
     <>
+      <section>
+        {tags.map((tag) => (
+          <div
+            onClick={() => {
+              setTagId(tag.id);
+            }}
+          >
+            {tag.name}
+          </div>
+        ))}
+      </section>
       {questionId && <EachQuestionPage questionId={questionId} />}
       {!questionId && (
         <>
