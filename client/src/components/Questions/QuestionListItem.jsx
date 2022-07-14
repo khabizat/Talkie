6dd74544fce69f481fec8fe7737688719a5ce6e0
JeongminJamie <sksrhcalsu@gmail.com>
2022-07-13 21:27:06 -0400

    delete button for all questions as well

--- client/src/components/Questions/QuestionListItem.jsx
+++ client/src/components/Questions/QuestionListItem.jsx
@@ -1,12 +1,50 @@
 import React from "react";
+import axios from "axios";
 
 export default function QuestionListItem(props) {
-  const { id, setQuestionId, name, date } = props;
+  const {
+    questionId,
+    user_id,
+    name,
+    date,
+    questions,
+    setQuestions,
+    setQuestionId,
+  } = props;
+
+  const findCreator = JSON.parse(localStorage.getItem("user"));
+  const creatorId = findCreator.id;
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+
+    axios
+      .delete(`/api/questions/${questionId}`)
+      .then((response) => {
+        const questionIndex = questions.findIndex((e) => e.id === questionId);
+        const thisQuestion = [...questions];
+        thisQuestion.splice(questionIndex, 1);
+        setQuestions(thisQuestion);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div>
-      <div onClick={() => setQuestionId(id)}>{name}</div>
-      <div>{date}</div>
+      {creatorId === user_id ? (
+        <>
+          <div onClick={() => setQuestionId(questionId)}>{name}</div>
+          <div>{date}</div>
+          <button onClick={handleDelete}>Delete</button>
+        </>
+      ) : (
+        <>
+          <div onClick={() => setQuestionId(questionId)}>{name}</div>
+          <div>{date}</div>
+        </>
+      )}
     </div>
   );
 }
