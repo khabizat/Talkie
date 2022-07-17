1f05090979c046aeddb51fb21be7f1734b5b7717
JeongminJamie <sksrhcalsu@gmail.com>
2022-07-17 14:35:14 -0400

    change background image and put the welcome section on the image

--- client/src/components/Home/HomeBanner.jsx
+++ client/src/components/Home/HomeBanner.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import banner from "../../Banner.jpg"
+import banner from "../../Home.jpg";
 
 export default function HomeBanner() {
   return (
     <div className="home-banner">
-      <img
-        src={banner}
-        alt={"Banner image"}
-      />
+      <img src={banner} alt={"Banner image"} />
     </div>
   );
 }
--- client/src/components/Home/HomeWelcome.jsx
+++ client/src/components/Home/HomeWelcome.jsx
@@ -12,17 +12,14 @@ export default function HomeWelcome() {
     <section className="main-content">
       <main className="home-welcome">
         <section className="welcome">
-          <h1 class="font-serif">Talkie</h1>
-          <h3 class="font-serif">
-            Take your interview prep to the next level
-          </h3>
+          <h3 class="font-serif">Take your interview prep to the next level</h3>
           <button
             onClick={handleStartButton}
             className="start-button"
             class="h-12 animate-pulse px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >Start
+          >
+            Start
           </button>
-
         </section>
       </main>
     </section>
