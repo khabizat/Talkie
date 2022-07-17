7c2c47617a6ee662fc21d007b5b95f0f9ea6385d
Khabizat Sharapudinova <habizat22@gmail.com>
2022-07-17 20:09:12 +0000

    Time formatting for user profile

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -67,8 +67,8 @@ module.exports = (db) => {
     const { userId } = req.params;
     db.query(
       `SELECT questions.name as question_name, 
-      questions.date as date, tags.name as tag_name
-      FROM questions 
+      TO_CHAR(questions.date, 'fmDay, fmMon DDth YYYY') as date,tags.name as tag_name
+      FROM questions
       JOIN tags
       ON questions.tag_id = tags.id
       WHERE user_id = $1`,
