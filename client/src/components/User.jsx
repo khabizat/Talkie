ae2ee2549edc47539063f89569f310db2fee5579
Khabizat Sharapudinova <habizat22@gmail.com>
2022-07-17 01:58:41 +0000

    Initial structure for User Profile

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -49,33 +49,96 @@ export default function User() {
 
   return (
     <section>
-      <div>My Questions</div>
-      {myQuestions &&
+      <div className="text-2xl text-center font-bold text-gray-600 hover:text-gray-600">
+        My Questions
+      </div>
+      {/* My Questions Container */}
+      <div className="p-4 w-full flex flex-wrap text-left bg-white border sm:p-8 mb-4">
+        {myQuestions &&
         myQuestions.map((myQuestion) => (
           <>
-            <div>Question: {myQuestion.question_name}</div>
-            <div>Tag: {myQuestion.tag_name}</div>
-            <div>Date: {myQuestion.date}</div>
+            {/* Header */}
+              <div className="p-4 w-1/2 text-left bg-blue-200 rounded-lg border shadow-md sm:p-8">
+                <div className="flex items-center justify-between">
+                  <span className="text-xs text-neutral-500">Posted on {myQuestion.date}</span>
+                </div>
+              {/* Body */}
+              <div className="flex items-center justify-between">
+                <p className="text-xl font-bold text-gray-600 hover:text-gray-600">
+                {myQuestion.question_name}
+                </p>
+              </div>
+              {/* Footer */}
+              <div className="flex items-center justify-between">
+                <button className="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold text-neutral-500">
+                {myQuestion.tag_name}
+                </button>
+              </div>
+            </div>
           </>
         ))}
-      <div>My Answers</div>
+      </div>
+
+      {/* My Answers */}
+      <div className="text-2xl text-center font-bold text-gray-600 hover:text-gray-600">
+        My Answers
+      </div>
+      <div className="p-4 w-full flex flex-wrap text-center bg-white border sm:p-8 mb-4">
       {myAnswers &&
         myAnswers.map((myAnswer) => (
           <>
-            <div>Answer: {myAnswer.answer_audio}</div>
-            <div>Question: {myAnswer.question_name}</div>
-            <div>Tag: {myAnswer.tag_name}</div>
-            <div>Date: {myAnswer.date}</div>
+            {/* Header */}
+            <div className="p-4 w-1/2 text-left bg-blue-200 rounded-lg border shadow-md sm:p-8">
+              <div className="flex items-center justify-between">
+                <span className="text-xs text-neutral-500">Posted on {myAnswer.date}</span>
+              </div>
+            {/* Body */}
+            <div className="flex items-center justify-between">
+              <p className="text-xl font-bold text-gray-600 hover:text-gray-600">
+                {myAnswer.question_name}
+              </p>
+            </div>
+          {/* Footer */}
+            <div className="flex items-center justify-between">
+              <button className="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold text-neutral-500">
+                {myAnswer.tag_name}
+              </button>
+            </div>
+          <div className="flex items-center justify-between mb-4">
+            <span>{myAnswer.answer_audio}</span>
+          </div>
+          </div>
           </>
         ))}
-      <div>Favorites Questions</div>
-      {favourites &&
+      </div>
+
+      {/* Favourite Questions */}
+      <div className="text-2xl text-center font-bold text-gray-600 hover:text-gray-600">
+        Favourite Questions
+      </div>
+      <div className="p-4 w-full flex flex-wrap text-center bg-white border shadow-md sm:p-8">
+        {favourites &&
         favourites.map((favourite) => (
           <>
-            <div>Question: {favourite.question_name}</div>
-            <div>Tag: {favourite.tag_name}</div>
+            {/* Header */}
+            <div className="p-4 w-1/2 text-left bg-blue-200 rounded-lg border shadow-md sm:p-8">
+            {/* Body */}
+            <div className="flex items-center justify-between">
+            <p className="text-xl font-bold text-gray-600 hover:text-gray-600">
+            {favourite.question_name}
+            </p>
+          </div>
+          {/* Footer */}
+          <div className="flex items-center justify-between">
+              <button className="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold text-neutral-500">
+              {favourite.tag_name}
+              </button>
+            </div>
+          </div>
           </>
         ))}
+      </div>
     </section>
   );
 }
+
