1ec264d064a600dc28d6e43a0626bb24b01b6ed2
Khabizat Sharapudinova <habizat22@gmail.com>
2022-07-18 02:07:25 +0000

    Modify QuestionsPage containers. Add photo

--- client/src/components/Questions/QuestionListItem.jsx
+++ client/src/components/Questions/QuestionListItem.jsx
@@ -10,6 +10,7 @@ export default function QuestionListItem(props) {
     name,
     date,
     tag,
+    photo,
     user_name,
     tag_name,
     questions,
@@ -54,6 +55,7 @@ export default function QuestionListItem(props) {
       .catch((err) => console.log(err));
   };
 
+
   return (
     <>
       {/* If user created this question */}
@@ -61,26 +63,38 @@ export default function QuestionListItem(props) {
         <>
           <div className="max-w-2xl w-9/12 px-8 py-4 mx-auto bg-blue-50 rounded-lg border p-5 shadow-lg transition hover:bg-blue-100 hover:border-blue-100 hover:scale-105">
             {/* Header of the container */}
-            <div className="flex items-center justify-between">
-              <div className="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://images.pexels.com/photos/430207/pexels-photo-430207.jpeg')]"></div>
-              <span className="text-xs text-neutral-500">{user_name}</span>
-              <span className="text-xs text-neutral-500">{dateFormatted}</span>
-              <button onClick={handleDelete}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 hover:fill-red-600"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={1}
-                >
+            <div>
+              {/* Header left */}
+            <div className="flex items-center justify-start">
+              <div>
+                <img src={photo} alt="Avatar" className="rounded-full h-12 w-12 bg-slate-400"/>
+              </div>
+
+              {/* Username with date */}
+              <div className="grid mx-2 w-1/3">
+                <span className="text-xs text-neutral-500 font-bold">You posted</span>
+                <span className="text-xs text-neutral-500">{dateFormatted}</span>
+              </div>
+              {/* Header right / Delete button */}
+              <div className="flex w-2/3 justify-end">
+                <button onClick={handleDelete}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 hover:fill-red-600"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={1}
+                  >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                   />
-                </svg>
-              </button>
+                  </svg>
+                </button>
+              </div>
+            </div>
             </div>
             {/* Body of the container */}
             <div className="mt-2">
@@ -90,22 +104,6 @@ export default function QuestionListItem(props) {
             </div>
             {/* Footer of the container */}
             <div className="flex items-center justify-between mt-4">
-              <button onClick={handleHeart}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 hover:fill-red-700"
-                  fill={heart ? "fill-red-700" : "none"}
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={1}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
-              </button>
               <button className="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold  text-neutral-500">
                 {tag_name}
               </button>
@@ -121,13 +119,40 @@ export default function QuestionListItem(props) {
           </div>
         </>
       ) : (
-        //If user didn't created this question
-        <div className="max-w-2xl w-9/12 px-8 py-4 mx-auto bg-blue-50 rounded-lg border p-5 shadow-lg transition hover:bg-blue-100 hover:border-blue-100 hover:scale-105">
+        //If user didn't create this question
+          <div className="max-w-2xl w-9/12 px-8 py-4 mx-auto bg-blue-50 rounded-lg border p-5 shadow-lg transition hover:bg-blue-100 hover:border-blue-100 hover:scale-105">
           {/* Header of the container */}
-          <div className="flex items-center justify-between">
-            <div className="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://images.pexels.com/photos/430207/pexels-photo-430207.jpeg')]"></div>
-            <span className="text-xs text-neutral-500">{user_name}</span>
-            <span className="text-xs text-neutral-500">{dateFormatted}</span>
+          <div>
+            <div className="flex items-center justify-start">
+              {/* Header left */}
+              <div>
+                <img src={photo} alt="Avatar" className="rounded-full h-12 w-12 bg-slate-400"/>
+              </div>
+              {/* Header username and date */}
+              <div className="grid mx-2 w-1/3">
+                <span className="text-xs text-neutral-500 font-bold">{user_name}</span>
+                <span className="text-xs text-neutral-500">{dateFormatted}</span>
+              </div>
+              {/* Header right / Like icon */}
+              <div className="flex w-2/3 justify-end">
+                <button onClick={handleHeart}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={"h-6 w-6 hover:fill-red-700"}
+                    fill={heart ? "fill-red-700" : "none"}
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={1}
+                  >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                  </svg>
+                </button>
+              </div>
+            </div>
           </div>
           {/* Body of the container */}
           <div className="mt-2">
@@ -137,25 +162,10 @@ export default function QuestionListItem(props) {
           </div>
           {/* Footer of the container */}
           <div className="flex items-center justify-between mt-4">
-            <button onClick={handleHeart}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={"h-6 w-6 hover:fill-red-700"}
-                fill={heart ? "fill-red-700" : "none"}
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={1}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-            </button>
             <button className="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold  text-neutral-500">
               {tag_name}
             </button>
+            {/* Listen answers button */}
             <div className="flex items-center">
               <button
                 onClick={() => setQuestionId(questionId)}
