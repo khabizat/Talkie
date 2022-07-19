1d6e11ad9dd40c1f657a2ecb8d39827d2a5fc9c1
JeongminJamie <sksrhcalsu@gmail.com>
2022-07-19 00:11:39 -0400

    changed the img url for users in db

--- server/db/seeds/seeds.sql
+++ server/db/seeds/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (name, email, password, photo_url)
 VALUES
 ('Jeongmin', 'test1@test.com', '12345', 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg'),
-('Kabby', 'test2@test.com', '123456', 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg'), 
-('Muhammad', 'test3@test.com', '1234567', 'https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg'),
+('Kabby', 'test2@test.com', '123456', 'https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg'), 
+('Muhammad', 'test3@test.com', '1234567', 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg'),
 ('Alice Jackson', 'alicejackson@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg'),
 ('Anthony Marlou', 'marlou@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://images.pexels.com/photos/428333/pexels-photo-428333.jpeg'),
 ('Katia Cheng', 'katiacheng@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://images.pexels.com/photos/1181690/pexels-photo-1181690.jpeg'),
