8872a68d9fd3c8987a47aec674dd765cc374c20d
JeongminJamie <sksrhcalsu@gmail.com>
2022-07-18 23:33:53 -0400

    add heart color change for tag question list item as well

--- client/src/components/Questions/QuestionListItem.jsx
+++ client/src/components/Questions/QuestionListItem.jsx
@@ -154,7 +154,7 @@ export default function QuestionListItem(props) {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className={"h-6 w-6 hover:fill-red-700"}
-                    fill={heart ? "fill-red-700" : "none"}
+                    fill={heart ? "red" : "none"}
                     viewBox="0 0 24 24"
                     stroke="currentColor"
                     strokeWidth={1}
