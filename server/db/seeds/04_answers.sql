f2d1bbb6a19a9766561ac57a460dc8a72ccac277
JeongminJamie <sksrhcalsu@gmail.com>
2022-07-18 22:06:12 -0400

    added audio files to database

--- server/db/seeds/04_answers.sql
+++ server/db/seeds/04_answers.sql
@@ -1,13 +1,13 @@
 INSERT INTO answers (audio_url, user_id, question_id, date)
 VALUES
 
-('https://talkieproject.s3.amazonaws.com/Audio//1658180405683-testAudio.mp3', 1, 1, '2022-07-11'),
-('I would approach this...', 2, 2, '2022-07-01'),
-('Because reasons!', 3, 3, '2022-11-01'),
-('Because reasons', 4, 4, '2022-04-21'),
-('I have no idea! However I would still like to try...', 5, 4, '2022-07-01'),
-('Sorry, I forgot to do my brain stretches.', 5, 5, '2022-04-30'),
-('erm, maybe?!', 6, 6, '2022-06-26'),
-('First I would create an object containing key value 🍐s', 7, 6, '2022-07-01'),
-('Do not ask me, I am just here for the free coffee... wait, there is free coffee right?', 7, 3, '2022-07-01'),
-('Because reasons', 8, 1, '2022-07-01');
+('https://talkieproject.s3.amazonaws.com/Audio//1658186855734-testAudio.mp3', 1, 1, '2022-07-11'),
+('https://talkieproject.s3.amazonaws.com/Audio//1658180405683-testAudio.mp3', 2, 2, '2022-07-01'),
+('https://talkieproject.s3.amazonaws.com/Audio//1658186957126-testAudio.mp3', 3, 3, '2022-11-01'),
+('https://talkieproject.s3.amazonaws.com/Audio//1658187133810-testAudio.mp3', 4, 4, '2022-04-21'),
+('https://talkieproject.s3.amazonaws.com/Audio//1658187211977-testAudio.mp3', 5, 4, '2022-07-01'),
+('https://talkieproject.s3.amazonaws.com/Audio//1658180405683-testAudio.mp3', 5, 5, '2022-04-30'),
+('https://talkieproject.s3.amazonaws.com/Audio//1658180405683-testAudio.mp3', 6, 6, '2022-06-26'),
+('https://talkieproject.s3.amazonaws.com/Audio//1658180405683-testAudio.mp3', 7, 6, '2022-07-01'),
+('https://talkieproject.s3.amazonaws.com/Audio//1658180405683-testAudio.mp3', 7, 3, '2022-07-23'),
+('https://talkieproject.s3.amazonaws.com/Audio//1658180405683-testAudio.mp3', 8, 1, '2022-07-11');
