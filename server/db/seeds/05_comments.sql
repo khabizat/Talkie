900ca7fcde211e8e38323e477b656bccd4a6d052
mdzwink <mdzwink@gmail.com>
2022-07-19 15:14:07 -0400

    adjust db seeds for presentation

--- server/db/seeds/05_comments.sql
+++ server/db/seeds/05_comments.sql
@@ -1,7 +1,7 @@
 INSERT INTO comments (user_id, name,  answer_id, comment, date)
 VALUES
 
-(3, 'Daniel Smith', 4, 'I think this is the best answer of all time! You are a genius', '1995-01-01'),
+(3, 'Daniel Smith', 4, 'I think this is the best answer of all time! You are a genius', '2022-07-10'),
 (3, 'Bella Scott', 3, 'This has truly inspired me!', '1995-01-01'),
 (4, 'Alice Jackson', 6, 'I have no clue what you are saying?', '1995-01-01'),
 (5, 'Anthony Marlou', 8, 'I think this is the best answer of all time! You are a genius', '2021-10-03'),
@@ -9,4 +9,4 @@ VALUES
 (7, 'Michael Weiss', 2, '🤔🤔', '2022-05-03'),
 (8, 'Laura Reynolds', 1, 'I think this is the best answer of all time! You are a genius', '2021-10-03'),
 (8, 'Michael Weiss', 7, '🤯🤯🤯', '2022-08-2'),
-(3, 'Daniel Smith', 5, 'I think this is the best answer of all time! You are a genius', '2022-');
+(3, 'Daniel Smith', 5, 'I think this is the best answer of all time! You are a genius', '2022-07-10');
